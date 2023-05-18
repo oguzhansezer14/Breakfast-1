d7ca7eb2950bb708c613b223b3d7cff48bc4b146
oguzhansezer14 <129849426+oguzhansezer14@users.noreply.github.com>
2023-05-18 13:14:16 +0000

    test project is added. Cleanup

---
+++ BuberBreakfast.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using BuberBreakfast.Services.Breakfasts;
+using Xunit;
+namespace BuberBreakfast.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+
+    [Fact]
+    public void GetMenu_Should_Return_Menu()
+    {
+        // Arrange
+        var service = new BreakfastService();
+
+        // Act
+        // var menu = service.GetMenu();
+
+        // Assert
+        // Assert.NotNull(menu);
+        // Assert.NotEmpty(menu.Items);
+        // Assert.Contains(menu.Items, item => item.Name == "Scrambled Eggs");
+        // Assert.Contains(menu.Items, item => item.Name == "Pancakes");
+        // Assert.Contains(menu.Items, item => item.Name == "Bacon");
+    }
+}
